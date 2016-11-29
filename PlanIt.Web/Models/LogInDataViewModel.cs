8070fdf46d8dd8f2392b53f4968ced37d376fecb
Olesia <Olesia@DESKTOP-M4JQSDP>
2016-11-29 18:40:26 +0200

    Edited LogIn and SignUp view models

--- PlanIt.Web/Models/LogInDataViewModel.cs
+++ PlanIt.Web/Models/LogInDataViewModel.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace PlanIt.Web.Models
 {
     public class LogInDataViewModel
     {
+        [Required(ErrorMessage = "Please enter your email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your passqord")]
         public string Password { get; set; }
     }
 }
--- PlanIt.Web/Models/SignUpDataViewModel.cs
+++ PlanIt.Web/Models/SignUpDataViewModel.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace PlanIt.Web.Models
 {
     public class SignUpDataViewModel
     {
+        [Required(ErrorMessage = "Please enter your email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Please enter your passqord")]
         public string Password { get; set; }
 
+        [Required(ErrorMessage = "Please enter your passqord one more time")]
         public string ConfirmPassword { get; set; }
     }
 }
