55ee52f3782d8478cae27b5da90099aa3551a17c
Olesia <Olesia@DESKTOP-M4JQSDP>
2016-11-29 15:32:43 +0200

    UserController

---
+++ PlanIt.Web/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PlanIt.Services.Abstract;
+
+namespace PlanIt.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public ViewResult SignUp()
+        {
+            return View();
+        }
+
+        public ViewResult SignIn()
+        {
+            return View();
+        }
+
+        public ViewResult EditProfile()
+        {
+            return View();
+        }
+
+        public ViewResult LogOut()
+        {
+            return View();
+        }
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
