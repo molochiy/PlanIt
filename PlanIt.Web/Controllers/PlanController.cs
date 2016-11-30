5868f6649336b81b88448b7b9c23e1b0b5ca041f
Mykhailo <kopylets2580@gmail.com>
2016-11-30 18:53:19 +0200

    added redirect

--- PlanIt.Web/Controllers/PlanController.cs
+++ PlanIt.Web/Controllers/PlanController.cs
@@ -1,28 +1,40 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
+using System.Web.Mvc;
 using PlanIt.Services.Abstract;
 using PlanIt.Web.Models;
 using System.Linq;
 using PlanIt.Entities;
 using System;
+using System.Web.Security;
 
 namespace PlanIt.Web.Controllers
 {
     public class PlanController : Controller
     {
         private readonly IPlanService _planService;
+        private readonly IUserService _userService;
 
-        public PlanController(IPlanService planService)
+        public PlanController(IPlanService planService, IUserService userService)
         {
             _planService = planService;
+            _userService = userService;
         }
 
         public ActionResult Index()
         {
-            int UserID = 1;
-            return View(new PlanIndexViewModel
+            try
             {
-                Plans = _planService.GetAllPlansByUserId(UserID)
-            });
+                User user = _userService.GetUserExistByEmail(HttpContext.User.Identity.Name);
+                return View(new PlanIndexViewModel
+                {
+                    Plans = _planService.GetAllPlansByUserId(user.Id)
+                });
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("LogIn", "User");
+            }
+                
         }
 
         public ActionResult AddPlan()
@@ -33,17 +45,26 @@ namespace PlanIt.Web.Controllers
         [HttpPost]
         public RedirectToRouteResult AddPlan(PlanAddPlanViewModel postData)
         {
-            _planService.SavePlan(new Plan {
-                Title = postData.Title,
-                Description = postData.Description,
-                Begin = postData.StartDate,
-                End = postData.EndDate,
-                StatusId = 1,
-                IsDeleted = false,
-                UserId = 1             
-            });
+            try
+            {
+                User user = _userService.GetUserExistByEmail(HttpContext.User.Identity.Name);
+                _planService.SavePlan(new Plan
+                {
+                    Title = postData.Title,
+                    Description = postData.Description,
+                    Begin = postData.StartDate,
+                    End = postData.EndDate,
+                    StatusId = 1,
+                    IsDeleted = false,
+                    UserId = user.Id
+                });
 
-            return RedirectToAction("Index");
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("LogIn", "User");
+            }
         }
     }
 }
