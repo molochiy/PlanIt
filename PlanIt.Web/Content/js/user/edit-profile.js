6977ead22ae7bf3a0b39f5a76af5f39cdcb0751a
bogdan <bogdan@DESKTOP-6427ESB>
2016-12-04 13:12:01 +0200

    add user menu

---
+++ PlanIt.Web/Content/js/user/edit-profile.js
@@ -0,0 +1,8 @@
+﻿$('#save').click(function () {
+    var form = $("#editProfileForm");
+    var url = form.attr("action");
+    var formData = form.serialize();
+    $.post(url, formData, function (data) {
+        $("#msg").html(data);
+    });
+})
---
+++ PlanIt.Web/Content/js/user/menu.js
@@ -0,0 +1,4 @@
+﻿$('.navbar-toggler').on('click', function (event) {
+    event.preventDefault();
+    $(this).closest('.navbar-minimal').toggleClass('open');
+})
