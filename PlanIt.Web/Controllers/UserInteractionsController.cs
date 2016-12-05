6157cf7f86c9f072fd9db1305b00b3a7541e5fac
Olesia <Olesia@DESKTOP-M4JQSDP>
2016-12-05 21:12:21 +0200

    Revert "Revert "share plan modal""
    This reverts commit 4804ab59ec0c106712955fcc684310fb0b9079a8.

---
+++ PlanIt.Web/Controllers/UserInteractionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlanIt.Web.Controllers
+{
+    public class UserInteractionsController : Controller
+    {
+        // GET: UserInteractions
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
