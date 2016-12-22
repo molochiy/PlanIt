b480f2d501a513eeb72731d698bcd18159e4989e
Mykola Molochii <molochiy@gmail.com>
2016-12-22 21:55:27 +0200

    Refactored UserService tests

--- PlanIt.Web.Tests/UserServiceTests.cs
+++ PlanIt.Web.Tests/UserServiceTests.cs
@@ -5,6 +5,7 @@ using Moq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PlanIt.Entities;
 using PlanIt.Repositories.Abstract;
+using PlanIt.Services.Abstract;
 using PlanIt.Services.Concrete;
 
 namespace PlanIt.Web.Tests
@@ -12,139 +13,153 @@ namespace PlanIt.Web.Tests
     [TestClass]
     public class UserServiceTests
     {
+        private Mock<IRepository> _mockRepository;
+        private IUserService _userService;
+        private IList<User> _users;
 
-        private IList<User> _users = new List<User>()
+        [TestInitialize]
+        public void Initialize()
         {
-            new User { Id = 1, Email = "e1@e.e", IsEmailConfirmed = true, Password = "pass1", ProfileId = 1},
-            new User { Id = 2, Email = "ee2@e.e", IsEmailConfirmed = true, Password = "pass2", ProfileId = 2},
-            new User { Id = 3, Email = "eee3@e.e", IsEmailConfirmed = false, Password = "pass3", ProfileId = 3},
-            new User { Id = 4, Email = "eeee4@e.e", IsEmailConfirmed = false, Password = "pass4", ProfileId = 4},
-            new User { Id = 5, Email = "eeeee5@e.e", IsEmailConfirmed = false, Password = "pass5", ProfileId = 5}
-        };
+            _mockRepository = new Mock<IRepository>();
+            _userService = new UserService(_mockRepository.Object);
+            _users = new List<User>
+            {
+                new User { Id = 1, Email = "e1@e.e", IsEmailConfirmed = true, Password = "pass1", ProfileId = 1},
+                new User { Id = 2, Email = "ee2@e.e", IsEmailConfirmed = true, Password = "pass2", ProfileId = 2},
+                new User { Id = 3, Email = "eee3@e.e", IsEmailConfirmed = false, Password = "pass3", ProfileId = 3},
+                new User { Id = 4, Email = "eeee4@e.e", IsEmailConfirmed = false, Password = "pass4", ProfileId = 4},
+                new User { Id = 5, Email = "eeeee5@e.e", IsEmailConfirmed = false, Password = "pass5", ProfileId = 5}
+            };
+        }
 
         [TestMethod]
         public void GetUserByIdExistTest()
         {
-            var mock = new Mock<IRepository>();
+            // Arrange
+            _mockRepository.Setup(rep => rep.GetSingle(It.IsAny<Func<User, bool>>())).Returns(_users[0]);
 
-            mock.Setup(rep => rep.GetSingle<User>(It.IsAny<Func<User, bool>>())).Returns(_users[0]);
+            // Act
+            var actualResult = _userService.GetUserById(1);
 
-            var userService = new UserService(mock.Object);
-
-            Assert.AreEqual(_users[0], userService.GetUserById(1));
+            // Assert
+            Assert.AreEqual(_users[0], actualResult);
         }
 
         [TestMethod]
         public void GetUserByIdNullTest()
         {
-            var mock = new Mock<IRepository>();
+            // Arrange
+            _mockRepository.Setup(rep => rep.GetSingle(It.IsAny<Func<User, bool>>())).Returns((User)null);
 
-            mock.Setup(rep => rep.GetSingle<User>(It.IsAny<Func<User, bool>>())).Returns((User)null);
+            // Act
+            var actualResult = _userService.GetUserById(0);
 
-            var userService = new UserService(mock.Object);
-
-            Assert.AreEqual(null, userService.GetUserById(0));
+            // Assert
+            Assert.AreEqual(null, actualResult);
         }
 
         [TestMethod]
         public void GetUserExistByEmailTest()
         {
-            var mock = new Mock<IRepository>();
+            // Arrange
+            _mockRepository.Setup(rep => rep.GetSingle(It.IsAny<Func<User, bool>>())).Returns((User)null);
 
-            mock.Setup(rep => rep.GetSingle<User>(It.IsAny<Func<User, bool>>())).Returns((User)null);
+            // Act
+            var actualResult = _userService.GetUserExistByEmail("e1@e.e");
 
-            var userService = new UserService(mock.Object);
-
-            Assert.AreEqual(null, userService.GetUserExistByEmail("e1@e.e"));
+            // Assert
+            Assert.AreEqual(null, actualResult);
         }
 
         [TestMethod]
         public void GetUserExistByEmailNullTest()
         {
-            var mock = new Mock<IRepository>();
+            // Arrange
+            _mockRepository.Setup(rep => rep.GetSingle(It.IsAny<Func<User, bool>>())).Returns((User)null);
 
-            mock.Setup(rep => rep.GetSingle<User>(It.IsAny<Func<User, bool>>())).Returns((User)null);
+            // Act
+            var actualResult = _userService.GetUserExistByEmail("");
 
-            var userService = new UserService(mock.Object);
-
-            Assert.AreEqual(null, userService.GetUserExistByEmail(""));
+            // Assert
+            Assert.AreEqual(null, actualResult);
         }
 
         [TestMethod]
         public void GetUserIdByEmailTest()
         {
-            var mock = new Mock<IRepository>();
+            // Arrange
+            _mockRepository.Setup(rep => rep.GetSingle(It.IsAny<Func<User, bool>>())).Returns(_users[0]);
 
-            mock.Setup(rep => rep.GetSingle<User>(It.IsAny<Func<User, bool>>())).Returns(_users[0]);
+            // Act
+            var actualResult = _userService.GetUserIdByEmail("e1@e.e");
 
-            var userService = new UserService(mock.Object);
-
-            Assert.AreEqual(1, userService.GetUserIdByEmail("e1@e.e"));
+            // Assert
+            Assert.AreEqual(1, actualResult);
         }
 
         [TestMethod]
         [ExpectedException(typeof(NullReferenceException))]
         public void GetUserIdByEmailNullRefExceptionTest()
         {
-            var mock = new Mock<IRepository>();
+            // Arrange
+            _mockRepository.Setup(rep => rep.GetSingle(It.IsAny<Func<User, bool>>())).Returns((User)null);
 
-            mock.Setup(rep => rep.GetSingle<User>(It.IsAny<Func<User, bool>>())).Returns((User)null);
-
-            var userService = new UserService(mock.Object);
-
-            userService.GetUserIdByEmail("");
+            // Act
+            _userService.GetUserIdByEmail("");
         }
 
         [TestMethod]
         public void GetUsersEmailsByEmailSubstringExceptCurrentUserTest()
         {
-            var mock = new Mock<IRepository>();
-
-            mock.Setup(rep => rep.Get<User>(It.IsAny<Func<User, bool>>())).Returns((List<User>) _users);
+            // Arrange
+            _mockRepository.Setup(rep => rep.Get(It.IsAny<Func<User, bool>>())).Returns((List<User>)_users);
             var currentUserEmail = "ee2@e.e";
             var expectedResult = _users.Select(u => u.Email).Where(email => email != currentUserEmail).ToList();
 
-            var userService = new UserService(mock.Object);
+            // Act
+            var actualResult = _userService.GetUsersEmailsByEmailSubstringExceptCurrentUser("", currentUserEmail);
 
-            var actualResult = userService.GetUsersEmailsByEmailSubstringExceptCurrentUser("", currentUserEmail);
-
+            // Assert
             Assert.IsTrue(expectedResult.SequenceEqual(actualResult));
         }
 
         [TestMethod]
         public void AddUserTest()
         {
-            var mock = new Mock<IRepository>();
+            // Arrange
+            _mockRepository.Setup(rep => rep.Insert(It.IsAny<User>())).Returns(_users[0]);
 
-            mock.Setup(rep => rep.Insert<User>(It.IsAny<User>())).Returns(_users[0]);
+            // Act
+            var actualResult = _userService.AddUser(_users[0]);
 
-            var userService = new UserService(mock.Object);
-
-            Assert.AreEqual(_users[0], userService.AddUser(_users[0]));
+            // Assert
+            Assert.AreEqual(_users[0], actualResult);
         }
 
         [TestMethod]
         public void UserExistsByEmailTrueTest()
         {
-            var mock = new Mock<IRepository>();
+            // Arrange
+            _mockRepository.Setup(rep => rep.Exists(It.IsAny<Func<User, bool>>())).Returns(true);
 
-            mock.Setup(rep => rep.Exists<User>(It.IsAny<Func<User, bool>>())).Returns(true);
+            // Act
+            var actualResult = _userService.UserExistsByEmail("e1@e.e");
 
-            var userService = new UserService(mock.Object);
-
-            Assert.IsTrue(userService.UserExistsByEmail("e1@e.e"));
+            // Assert
+            Assert.IsTrue(actualResult);
         }
 
         [TestMethod]
         public void UserExistsByEmailFalseTest()
         {
-            var mock = new Mock<IRepository>();
+            // Arrange
+            _mockRepository.Setup(rep => rep.Exists(It.IsAny<Func<User, bool>>())).Returns(false);
 
-            mock.Setup(rep => rep.Exists<User>(It.IsAny<Func<User, bool>>())).Returns(false);
+            // Act
+            var actualResult = _userService.UserExistsByEmail("");
 
-            var userService = new UserService(mock.Object);
-
-            Assert.IsFalse(userService.UserExistsByEmail(""));
+            // Assert
+            Assert.IsFalse(actualResult);
         }
     }
 }
