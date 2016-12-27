68efa67510e6d5a8bc4bbfe3c244390e1e02294c
Olesia <Olesia@DESKTOP-M4JQSDP>
2016-12-28 01:46:30 +0200

    remove participant

--- PlanIt.Web/Scripts/app/user-interactions/get-participants.js
+++ PlanIt.Web/Scripts/app/user-interactions/get-participants.js
@@ -5,7 +5,7 @@
         $.each(data, function (index, item) {
             dropdown.append(
                 $('<li role="presentation"></li>')).append(
-                $('<a role="menuitem" tabindex="-1" href="#"></a>').val(item).html(item)).append(
+                $('<a role="menuitem" tabindex="-1" href="#" onclick="removeParticipant(&quot;' + item + '&quot;,' + planId + ')"></a>').val(item).html(item)).append(
                 $('<span class="glyphicon glyphicon-remove pull-right"></span>'));
         });
     });
